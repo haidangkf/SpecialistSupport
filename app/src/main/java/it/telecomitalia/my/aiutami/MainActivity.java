0ef3202bfac17d8b04cf59633709b23dd3218c31
marco <mpao@me.com>
2016-01-29 14:18:50 +0100

    personalizzazione menu e colori

--- app/src/main/java/it/telecomitalia/my/aiutami/MainActivity.java
+++ app/src/main/java/it/telecomitalia/my/aiutami/MainActivity.java
@@ -108,6 +108,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            return true;
+        }
+        if (id == R.id.action_reload) {
+            return true;
+        }
         if (id == R.id.action_settings) {
             return true;
         }
@@ -125,17 +131,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
+            // Handle the action
+        } else if (id == R.id.nav_favs) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_question) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_answer) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_logout) {
+            logout();
+        } else if (id == R.id.cat_1) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.cat_2) {
 
         }
 
